8ded61e11467d4bd8e87b3e0691cd232deb8fce3
wladimir <wladimirburgos42@gmail.com>
2021-10-15 03:57:29 -0300

    cambios models

--- coaching/core/admin.py
+++ coaching/core/admin.py
@@ -5,10 +5,9 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Coachee)
-admin.site.register(Empresa)
-admin.site.register(Coach)
-admin.site.register(Encuesta)
-admin.site.register(Proceso)
-admin.site.register(Documentacion)
-admin.site.register(Contrato)
+#admin.site.register(Coachee)
+#admin.site.register(Empresa)
+#admin.site.register(Coach)
+#admin.site.register(Encuesta)
+#admin.site.register(Proceso)
+#admin.site.register(Documentacion)
