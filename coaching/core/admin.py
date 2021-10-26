6be8a831c4f999c4adfa8a3981504eadfd776fa0
wladimir <wladimirburgos42@gmail.com>
2021-10-26 04:03:42 -0300

    Cambios 26-10-21
    Cambios al models.
    Contraseña encriptada
    Admin modulo agrega todo

--- coaching/core/admin.py
+++ coaching/core/admin.py
@@ -5,9 +5,9 @@ from .models import *
 
 # Register your models here.
 
-#admin.site.register(Coachee)
-#admin.site.register(Empresa)
-#admin.site.register(Coach)
+admin.site.register(Coachee)
+admin.site.register(Empresa)
+admin.site.register(Coach)
 #admin.site.register(Encuesta)
-#admin.site.register(Proceso)
+admin.site.register(Proceso)
 #admin.site.register(Documentacion)
